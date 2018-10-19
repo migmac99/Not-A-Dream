554248c08681405582edccd99f9bbf1db036255b
migmac99 <migmacthegamer@gmail.com>
2018-10-19 16:52:44 +0100

    new link
    update

--- Not-A-Dream/Assets/PlayerMovement.cs
+++ Not-A-Dream/Assets/PlayerMovement.cs
@@ -7,18 +7,19 @@ public class PlayerMovement : MonoBehaviour {
 	public float xSpeed = 0f;
 	public float ySpeed = 0f;
 
-
 	// Use this for initialization
 	void Start () {
-		
+
 	}
-	
+
 	// Update is called once per frame
 	void Update () {
 
+		if (Input.GetKey (KeyCode.D)) {
+			this.transform.position += new Vector3 (xSpeed, ySpeed, 0);
+			xSpeed = 0.01f;
+			this.transform.
+		}
 
-		this.transform.position += new Vector3 (xSpeed,ySpeed,0);
-		xSpeed = 1f;
-		
 	}
 }
--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # Not-A-Dream
 
-https://insiders.liveshare.vsengsaas.visualstudio.com/join?0F544136A9F9692FF18FABED3C9B058D881F
+https://insiders.liveshare.vsengsaas.visualstudio.com/join?ECE352A00BD2CF61B2FA7B68A79074A69DA9
