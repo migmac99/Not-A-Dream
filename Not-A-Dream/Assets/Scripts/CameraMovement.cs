1a11fce739aae0ffa93a3d7a17a0a42bc690b63e
migmac99 <migmacthegamer@gmail.com>
2018-10-20 20:11:59 +0100

    updated work
    unity project updated
    
    + walk
    + jump
    + colision w floor
    + unfinished floor detection for camera

---
+++ Not-A-Dream/Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour {
+
+	private Vector2 velocity; //required for calculations
+
+	public float timer_y; //Timer for camera smoothing
+	public float timer_x;
+
+	public GameObject player;
+	public GameObject floor;
+
+	public float floorMargin; //Margin to adjust y axis on camera
+
+	private PlayerMovement playerMovement;
+
+	void Awake () {
+		playerMovement = GetComponent<PlayerMovement> ();
+	}
+
+	void Start () {
+		player = GameObject.FindGameObjectWithTag ("Player");
+	}
+
+	void Update () {
+		//floor = GameObject.Find (playerMovement.floorTag.ToString());
+		floor = playerMovement.floorTag;
+	}
+
+	void FixedUpdate () {
+
+		float position_x = Mathf.SmoothDamp (transform.position.x, player.transform.position.x, ref velocity.x, timer_x);
+		float position_y = Mathf.SmoothDamp (transform.position.y, (floor.transform.position.y + floorMargin), ref velocity.y, timer_y);
+
+		transform.position = new Vector3 (position_x, position_y, -1f);
+
+	}
+
+}
---
+++ Not-A-Dream/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+	public float Speed = 0f;
+	public float JumpForce = 0f;
+
+	private float moveInput;
+	private Rigidbody2D rb;
+	
+	private bool facingRight = true;
+	private bool isGrounded;
+	public Transform groundCheck;
+	public float checkRadius;
+	public LayerMask whatIsGround;
+
+	private int extraJumps;
+	public int extraJumpsValue;
+	public float jumpSpeed;
+
+	public GameObject floorTag;
+
+	void Awake () {
+		rb = GetComponent<Rigidbody2D> ();
+	}
+
+	void Start(){
+		extraJumps = extraJumpsValue;
+	}
+
+
+	// Update is called once per frame
+	void Update () {
+		KeyboardInput ();
+
+		if (isGrounded) {
+			extraJumps = extraJumpsValue;
+		}
+	}
+
+	void KeyboardInput () {
+
+		if (Input.GetKey (KeyCode.D)) {
+			this.transform.position += new Vector3 (Speed, 0, 0);
+			Speed = 0.01f;
+
+		}
+		if (Input.GetKey (KeyCode.A)) {
+			this.transform.position += new Vector3 (Speed, 0, 0);
+			Speed = -0.01f;
+
+		}
+		if (Input.GetKey (KeyCode.S)) {
+
+		}
+
+		if (Input.GetKeyDown (KeyCode.Space) && extraJumps > 0) {
+			rb.velocity = Vector2.up * JumpForce;
+			extraJumps -= 1;
+		} else if (Input.GetKeyDown (KeyCode.Space) && extraJumps == 0 && isGrounded) {
+			rb.velocity = Vector2.up * JumpForce;
+		}
+
+	}
+
+	void OnTriggerEnter2D (Collider2D other) {
+		floorTag = other.gameObject;
+	}
+
+	void Flip () {
+		facingRight = !facingRight;
+		Vector3 Scaler = transform.localScale;
+		Scaler.x *= -1;
+		transform.localScale = Scaler;
+	}
+
+	void FixedUpdate () {
+
+		isGrounded = Physics2D.OverlapCircle (groundCheck.position, checkRadius, whatIsGround);
+
+		if (facingRight == false && Speed > 0) {
+			Flip ();
+		} else if (facingRight == true && Speed < 0) {
+			Flip ();
+		}
+	}
+
+}
